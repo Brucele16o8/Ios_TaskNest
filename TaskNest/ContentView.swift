c494d9d626153ce2eadc019fa83eb8c77f72595b
Bruce Le <brucele.tt168@gmail.com>
2025-06-08 18:36:00 +1000

    Initial Commit

---
+++ TaskNest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskNest
+//
+//  Created by Tung Le on 8/6/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskNest/TaskNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskNestApp.swift
+//  TaskNest
+//
+//  Created by Tung Le on 8/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
